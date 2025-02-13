f3e9b1eb1e00c9e72950bec2f377af7922deddf7
Ilshat-AI007 <akhmetshinilshat2@gmail.com>
2025-02-13 19:43:07 +0500

    start 4

--- mysite/settings.py
+++ mysite/settings.py
@@ -91,7 +91,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'main/static'),  # Указывает на папку со статическими файлами
+    os.path.join(BASE_DIR, 'main/static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
